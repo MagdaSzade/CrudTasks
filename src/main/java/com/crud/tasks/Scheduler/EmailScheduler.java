dd87a75a2c9eaf5246c905337d3ecba34891890c
MagdaSzade <magdalenasuchanek@gmail.com>
2019-03-21 21:20:06 +0100

    24.3 - Zadanie

--- src/main/java/com/crud/tasks/Scheduler/EmailScheduler.java
+++ src/main/java/com/crud/tasks/Scheduler/EmailScheduler.java
@@ -3,8 +3,12 @@ package com.crud.tasks.Scheduler;
 import com.crud.tasks.config.AdminConfig;
 import com.crud.tasks.domain.Mail;
 import com.crud.tasks.repository.TaskRepository;
+import com.crud.tasks.service.MailCreatorService;
 import com.crud.tasks.service.SimpleEmailService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.mail.javamail.MimeMessagePreparator;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
@@ -16,20 +20,27 @@ public class EmailScheduler {
     private TaskRepository taskRepository;
     @Autowired
     private AdminConfig adminConfig;
+    @Autowired
+    private MailCreatorService mailCreatorService;
+    @Autowired
+    private JavaMailSender javaMailSender;
 
-    private static final String SUBJECT = "Tasks: once a day email";
 
     @Scheduled(cron = "0 0 10 * * *")
     public void sendInformationEmail() {
-        long size = taskRepository.count();
-        String endOfSentence = " tasks.";
-        if (size == 1) {
-            endOfSentence = " task.";
-        }
-        simpleEmailService.send(new Mail(
+        javaMailSender.send(createMimeMessage(new Mail(
                 adminConfig.getAdminMail(),
                 null,
-                SUBJECT,
-                "Currntly in database you got: " + size + endOfSentence));
-        }
+                "Size of Task List",
+                "")));
+    }
+
+    private MimeMessagePreparator createMimeMessage(final Mail mail) {
+        return mimeMessage -> {
+            MimeMessageHelper messageHelper = new MimeMessageHelper(mimeMessage);
+            messageHelper.setTo(mail.getMailTo());
+            messageHelper.setSubject(mail.getSubject());
+            messageHelper.setText(mailCreatorService.scheduledEmail(), true);
+        };
+    }
 }
--- src/main/java/com/crud/tasks/service/MailCreatorService.java
+++ src/main/java/com/crud/tasks/service/MailCreatorService.java
@@ -1,6 +1,7 @@
 package com.crud.tasks.service;
 
 import com.crud.tasks.config.AdminConfig;
+import com.crud.tasks.repository.TaskRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
@@ -17,16 +18,28 @@ public class MailCreatorService {
     @Autowired
     @Qualifier("templateEngine")
     private TemplateEngine templateEngine;
+    @Autowired
+    private TaskRepository taskRepository;
 
     public String buildTrelloCardEmail(String message) {
-
         List<String> functionality = new ArrayList<>();
         functionality.add("You can manage your task");
         functionality.add("Provides connection with Trello Account");
         functionality.add("Application allows sending task to Tesllo");
-
-        Context context = new Context();
+        Context context = getContext();
         context.setVariable("message", message);
+        context.setVariable("application_functionality", functionality);
+        return templateEngine.process("mail/created-trello-card-mail", context);
+    }
+
+    public String scheduledEmail() {
+        Context context = getContext();
+        context.setVariable("TaskListSize", taskRepository.count());
+        return templateEngine.process("mail/scheduled-email", context);
+    }
+
+    private Context getContext() {
+        Context context = new Context();
         context.setVariable("task_url", "http://localhost:8888/crud");
         context.setVariable("button", "Visit website");
         context.setVariable("admin_name", adminConfig.getAdminName());
@@ -38,7 +51,6 @@ public class MailCreatorService {
         context.setVariable("show_button",false);
         context.setVariable("is_friend",false);
         context.setVariable("admin_config", adminConfig);
-        context.setVariable("application_functionality", functionality);
-        return templateEngine.process("mail/created-trello-card-mail", context);
+        return context;
     }
 }
